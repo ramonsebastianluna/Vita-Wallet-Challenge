873c52b89914aef8d043635daabfe4524a68f94e
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-02-22 11:56:48 -0300

    feat: exchange inital mockup with logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from "./routes/AppRoutes";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './sass/App.scss'
 
 function App() {
