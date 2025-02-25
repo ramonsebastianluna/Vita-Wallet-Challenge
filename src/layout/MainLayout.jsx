d30dbdb0d9c02e491fe8309193430b153e15b4e9
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-02-25 01:41:04 -0300

    feat(aside): desktop styles added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,9 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const MainLayout = ({children}) => {
     const navigate = useNavigate();
+    const location = useLocation();
     const { setCredentials } = useAuth();
 
     const handleLogout = () => {
@@ -19,27 +20,37 @@ const MainLayout = ({children}) => {
         navigate('/', { replace: true });
     };
 
+    const menuItems = [
+        { path: "/inicio", label: "Inicio" },
+        { path: "/transferir", label: "Transferir" },
+        { path: "/recargar", label: "Recargar" },
+        { path: "/intercambiar", label: "Intercambiar" },
+        { path: "/perfil", label: "Perfil" },
+        { path: "/ayuda", label: "Ayuda" },
+    ];
+
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-12 col-sm-3 d-flex flex-column p-0">
-                    <aside className="aside">
+                <div className="col-12 col-sm-4 col-lg-3 d-flex flex-column p-0">
+                    <aside className="aside position-relative">
                         <div className="aside__toggle d-block d-sm-none">
                             <a className="btn border border-dark m-2" data-bs-toggle="offcanvas" href="#offcanvasMenu" role="button" aria-controls="offcanvasMenu">
                                 ☰
                             </a>
                         </div>
 
-                        <div className="aside__desktop d-none d-sm-block position-relative">
-                            <ul className="list-group mx-4">
-                                <li className="list-group-item"><Link to="/inicio">Inicio</Link></li>
-                                <li className="list-group-item"><Link to="/transferir">Transferir</Link></li>
-                                <li className="list-group-item"><Link to="/recargar">Recargar</Link></li>
-                                <li className="list-group-item"><Link to="/intercambiar">Intercambiar</Link></li>
-                                <li className="list-group-item"><Link to="/perfil">Perfil</Link></li>
-                                <li className="list-group-item"><Link to="/ayuda">Ayuda</Link></li>
+                        <div className="aside__desktop d-none d-sm-block">
+                            <ul className="list-menu-desktop f-family-open-sans-regular mx-0 ps-0">
+                                {menuItems.map(item => (
+                                    <li 
+                                        key={item.path} 
+                                        className={`list-menu-desktop__item fs-5 my-2 rounded-end-5 ${location.pathname === item.path ? "active" : ""}`}
+                                    >
+                                        <Link className="text-white" to={item.path}>{item.label}</Link>
+                                    </li>
+                                ))}
                             </ul>
-                            <button onClick={handleLogout}>Cerrar sesión</button>
                         </div>
 
                         <div className="aside__mobile offcanvas offcanvas-start w-75" tabIndex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
@@ -57,12 +68,14 @@ const MainLayout = ({children}) => {
                                         <li className="list-group-item"><Link to="/ayuda">Ayuda</Link></li>
                                     </ul>
                                 </div>
-                                <button onClick={handleLogout}>Cerrar sesión</button>
                             </div>
                         </div>
+
+                        <button className="btn btn-close-session f-family-open-sans-regular fs-5 text-white mx-4 p-0 position-absolute" onClick={handleLogout}>Cerrar sesión</button>
+
                     </aside>
                 </div>
-                <div className="col-12 col-sm-9">
+                <div className="col-12 col-sm-8 col-lg-9">
                     {children}
                 </div>
             </div>        
