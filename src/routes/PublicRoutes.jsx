42ca094f47f0931c569850b6202159b901a0b525
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-02-26 08:43:20 -0300

    Updates

--- src/routes/PublicRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import Login from "../pages/auth/Login";
-import Unauthorized from "../pages/auth/Unauthorized";
-
-const PublicRoutes = [
-    { path: "/", element: <Login />},
-    { path: "/no-autenticado", element: <Unauthorized />}
-]
-
-export default PublicRoutes;
