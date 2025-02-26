c107a8af6b4794d2986a20c442da10a15a1d0432
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-02-26 07:59:36 -0300

    feat: site in development view added

--- src/pages/dashboard/recharge/Recharge.jsx
+++ src/pages/dashboard/recharge/Recharge.jsx
@@ -1,7 +1,14 @@
+import viewOff from "/images/view-off.png";
+
 const Recharge = () => {
-  return (
-    <div>Recharge</div>
-  )
+    return (
+        <div className="container">
+            <div className="row justify-content-center align-items-center h-100dvh">
+                <img src={viewOff} alt="view-off" style={{ width: "370px", height: "370px" }} />
+                <p class="text-center f-family-open-sans-regular">UPS! Interior en desarrollo. Aun estamos trabajando en ello...</p>
+            </div>
+        </div>
+    )
 }
 
 export default Recharge
