9dd8624011a47b140e9e6ec43b07e2489a8de9d1
Seba Luna <ramon.sebastian.luna@gmail.com>
2025-03-07 00:40:10 -0300

    fix(Unauthorized): class by className

--- src/pages/auth/Unauthorized.jsx
+++ src/pages/auth/Unauthorized.jsx
@@ -8,7 +8,7 @@ const Unauthorized = () => {
         <div className="container">
             <div className="row justify-content-center align-items-center h-100dvh">
                 <img src={viewOff} alt="view-off" style={{ width: "370px", height: "370px" }} />
-                <p class="text-center f-family-open-sans-regular">UPS! No tienes permiso para ver este sitio...</p>
+                <p className="text-center f-family-open-sans-regular">UPS! No tienes permiso para ver este sitio...</p>
                 <button className="btn btn-back me-3 w-25" onClick={() => navigate("/")}>Ir al login</button>
             </div>
         </div>
